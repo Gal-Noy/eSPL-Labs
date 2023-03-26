01edc8364ee62e3835472fb41ebf28044f5930ce
Gal-Noy <galnoy1105@gmail.com>
2023-03-26 20:36:52 +0300

    finished labB part0

---
+++ labB/Part0/hexaPrint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintHex(unsigned char *buffer, int length)
+{
+    for (int i = 0; i < length; i++)
+        printf("%02X ", buffer[i]);
+}
+int main(int argc, char **argv)
+{
+    FILE *f;
+    int length;
+    unsigned char *buffer;
+
+    f = fopen(argv[1], "r");
+    if (!f)
+    {
+        fprintf(stderr, "Error: could not open file");
+        return 0;
+    }
+
+    buffer = malloc(sizeof(int) * 256);
+    length = fread(buffer, sizeof(char), 256, f);
+    PrintHex(buffer, length);
+
+    fclose(f);
+    free(buffer);
+}
