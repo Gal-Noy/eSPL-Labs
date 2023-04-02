e5955821a636dc98d3c1ac1fe8220592ad1bda66
Gal-Noy <galnoy1105@gmail.com>
2023-04-02 19:34:18 +0300

    finished part0 lab4

---
+++ lab4/Task0/myshell.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <linux/limits.h>
+#include "LineParser.h"
+
+void handler(int);
+
+void execute(cmdLine *pCmdLine)
+{
+    if (strcmp(pCmdLine->arguments[0], "quit") == 0)
+    {
+        freeCmdLines(pCmdLine);
+        exit(1);
+    }
+    if (execv(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
+    {
+        perror("execv() error");
+        freeCmdLines(pCmdLine);
+        _exit(0);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    char curr_dir[PATH_MAX], line[2048];
+    cmdLine *pCmdLine;
+
+    while (1)
+    {
+        getcwd(curr_dir, PATH_MAX);
+        printf("%s\n", curr_dir);
+
+        fgets(line, 2048, stdin);
+        pCmdLine = parseCmdLines(line);
+
+        execute(pCmdLine);
+
+        freeCmdLines(pCmdLine);
+    }
+
+    return 0;
+}
