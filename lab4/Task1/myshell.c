1295aa0bc3f2e5c4a3156d87fac49aeca504e8bb
Gal-Noy <galnoy1105@gmail.com>
2023-04-02 21:07:18 +0300

    add io to lab4

--- lab4/Task1/myshell.c
+++ lab4/Task1/myshell.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <linux/limits.h>
@@ -12,13 +13,16 @@ void exit_program(cmdLine *pCmdLine, int code, char *error)
 {
     freeCmdLines(pCmdLine);
     if (error)
+    {
         perror(error);
+        free(error);
+    }
     exit(code);
 }
 
 void execute(cmdLine *pCmdLine, int debug)
 {
-    int child_pid;
+    int child_pid, input = -1, output = -1;
 
     if (strcmp(pCmdLine->arguments[0], "quit") == 0)
         exit_program(pCmdLine, 1, NULL);
@@ -30,11 +34,34 @@ void execute(cmdLine *pCmdLine, int debug)
     }
 
     child_pid = fork();
+
     if (child_pid == -1)
         exit_program(pCmdLine, 0, "fork() error");
     else if (child_pid == 0)
+    {
+        if (pCmdLine->inputRedirect)
+        {
+            input = open(pCmdLine->inputRedirect, O_RDONLY);
+            if (input == -1)
+                exit_program(pCmdLine, 0, "open() inputRedirect error");
+            if (dup2(input, STDIN_FILENO) == -1)
+                exit_program(pCmdLine, 0, "dup2() inputRedirect error");
+            close(input);
+        }
+
+        if (pCmdLine->outputRedirect)
+        {
+            output = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (output == -1)
+                exit_program(pCmdLine, 0, "open() outputRedirect error");
+            if (dup2(output, STDOUT_FILENO) == -1)
+                exit_program(pCmdLine, 0, "dup2() outputRedirect error");
+            close(output);
+        }
+
         if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
             exit_program(pCmdLine, 0, "execvp() error");
+    }
 
     if (debug)
         fprintf(stderr, "PID: %d\nExecuting command: %s\n", child_pid, pCmdLine->arguments[0]);
@@ -56,7 +83,7 @@ int main(int argc, char **argv)
     while (1)
     {
         getcwd(curr_dir, PATH_MAX);
-        printf("%s\n", curr_dir);
+        printf("%s : ", curr_dir);
 
         fgets(line, 2048, stdin);
         pCmdLine = parseCmdLines(line);
