67964836128bc39031cff4ce483d505ced407adf
Gal-Noy <galnoy1105@gmail.com>
2023-04-15 22:11:16 +0300

    finished part1 labC

---
+++ lab5(C)/Part1/mypipeline.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+    int pipefd[2], pid1, pid2;
+    char *ls_args[] = {"ls", "-l", 0};
+    char *tail_args[] = {"tail", "-n", "2", 0};
+
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe error.");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stderr, "(parent_process>forking...)\n");
+    pid1 = fork();
+    switch (pid1)
+    {
+    case -1:
+        perror("child1 fork error.");
+        exit(EXIT_FAILURE);
+        break;
+    case 0: // In the child1 process
+        close(STDOUT_FILENO);
+        dup(pipefd[1]);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        fprintf(stderr, "(child1>redirecting stdout to the write end of the pipe...)\n");
+        fprintf(stderr, "(child1>going to execute cmd: ls -l)\n");
+        execvp(ls_args[0], ls_args);
+        exit(EXIT_FAILURE);
+        break;
+    default: // In the parent process
+        fprintf(stderr, "(parent_process>created process with id: %d)\n", pid1);
+        close(pipefd[1]);
+        fprintf(stderr, "(parent_process>closing the write end of the pipe...)\n");
+    }
+
+    fprintf(stderr, "(parent_process>forking...)\n");
+    pid2 = fork();
+    switch (pid2)
+    {
+    case -1:
+        perror("child2 fork error.");
+        exit(EXIT_FAILURE);
+        break;
+    case 0: // In the child2 process
+        close(STDIN_FILENO);
+        dup(pipefd[0]);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        fprintf(stderr, "(child2>redirecting stdin to the read end of the pipe...)\n");
+        fprintf(stderr, "(child2>going to execute cmd: tail -n 2)\n");
+        execvp(tail_args[0], tail_args);
+        exit(EXIT_FAILURE);
+        break;
+    default: // In the parent process
+        fprintf(stderr, "(parent_process>created process with id: %d)\n", pid2);
+        close(pipefd[0]);
+        fprintf(stderr, "(parent_process>closing the read end of the pipe...)\n");
+    }
+
+    // Wait for child processes to terminate
+    fprintf(stderr, "(parent_process>waiting for child processes to terminate...)\n");
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    fprintf(stderr, "(parent_process>exiting...)\n");
+
+    return 0;
+}
---
+++ lab5(C)/Task4/myshell.c
@@ -0,0 +1,127 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <linux/limits.h>
+#include "LineParser.h"
+
+#define LINE_SIZE 2048
+
+int debug = 0;
+
+void exit_program(cmdLine *pCmdLine, int code, char *error, int use_exit)
+{
+    if (error)
+        perror(error);
+    freeCmdLines(pCmdLine);
+    if (use_exit)
+        exit(code);
+    else
+        _exit(code);
+}
+void change_directory(cmdLine *pCmdLine)
+{
+    if (chdir(pCmdLine->arguments[1]) < 0)
+        exit_program(pCmdLine, 0, "chdir() error", 0);
+}
+void signal_process(cmdLine *pCmdLine, int signal)
+{
+    int child_pid = atoi(pCmdLine->arguments[1]);
+    if (kill(child_pid, signal) < 0)
+        exit_program(pCmdLine, 0, "kill() error", 0);
+    printf("Process %d has been %s.\n", child_pid, signal == SIGTSTP ? "suspended" : signal == SIGCONT ? "woken up"
+                                                                                                       : "terminated");
+}
+void io_process(cmdLine *pCmdLine, const char *fd, int flags, int stdfd, char *error)
+{
+    if (fd)
+    {
+        int res = open(fd, flags, 0644);
+        if (res == -1)
+            exit_program(pCmdLine, 0, error, 0);
+        if (dup2(res, stdfd) == -1)
+            exit_program(pCmdLine, 0, error, 0);
+        close(res);
+    }
+}
+
+void execute(cmdLine *pCmdLine)
+{
+    int child_pid;
+    char *arg = pCmdLine->arguments[0];
+
+    if (strcmp(arg, "quit") == 0)
+        exit_program(pCmdLine, 1, NULL, 0);
+    else if (strcmp(arg, "cd") == 0)
+    {
+        change_directory(pCmdLine);
+        return;
+    }
+    else if (strcmp(arg, "suspend") == 0)
+    {
+        signal_process(pCmdLine, SIGTSTP);
+        return;
+    }
+    else if (strcmp(arg, "wake") == 0)
+    {
+        signal_process(pCmdLine, SIGCONT);
+        return;
+    }
+    else if (strcmp(arg, "kill") == 0)
+    {
+        signal_process(pCmdLine, SIGINT);
+        return;
+    }
+
+    child_pid = fork();
+
+    switch (child_pid)
+    {
+    case -1:
+        exit_program(pCmdLine, 0, "fork() error", 0);
+        break;
+    case 0:
+        io_process(pCmdLine, pCmdLine->inputRedirect, O_RDONLY, STDIN_FILENO, "open() inputRedirect error");
+        io_process(pCmdLine, pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, STDOUT_FILENO, "open() outputRedirect error");
+        if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
+            exit_program(pCmdLine, 0, "execvp() error", 1);
+        break;
+    }
+
+    if (debug)
+        fprintf(stderr, "PID: %d\nExecuting command: %s\n", child_pid, arg);
+
+    if (pCmdLine->blocking)
+        waitpid(child_pid, NULL, 0);
+}
+
+int main(int argc, char **argv)
+{
+    char curr_dir[PATH_MAX], line[LINE_SIZE];
+    cmdLine *pCmdLine;
+
+    for (int i = 0; i < argc; i++)
+        if (strcmp(argv[i], "-d") == 0)
+            debug = 1;
+
+    while (1)
+    {
+        getcwd(curr_dir, PATH_MAX);
+        printf("%s : ", curr_dir);
+
+        fgets(line, LINE_SIZE, stdin);
+        pCmdLine = parseCmdLines(line);
+        if (!pCmdLine)
+            continue;
+
+        execute(pCmdLine);
+
+        freeCmdLines(pCmdLine);
+    }
+
+    return 0;
+}
