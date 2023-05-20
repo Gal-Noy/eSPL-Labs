b67eba683a6ea31f981c52e74f3c0c76a38e1109
Gal-Noy <galnoy1105@gmail.com>
2023-05-20 21:34:54 +0300

    modify instructions.txt in Task2 lab8

--- lab8(4)/Task4/task4.c
+++ lab8(4)/Task4/task4.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char **argv)
+int digit_cnt(char *s)
 {
+    int i = 0, count = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+            count++;
+        i++;
+    }
+    return count;
+}
+
+int main(int argc, char *argv[])
+{
+    int count;
+
+    if (argc < 2)
+    {
+        printf("Usage: ./digit_ctr <string>\n");
+        return 1;
+    }
+
+    count = digit_cnt(argv[1]);
+    printf("Number of digits: %d\n", count);
+
     return 0;
 }
